fd9f3661afd4f8d2d0fedf8aff390d87c5d9af33
Adam <adam.thorpe.g@gmail.com>
2014-05-21 14:20:34 -0600

    feat: add variable discovery functions

--- bin/unit.js
+++ bin/unit.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 var make = exports.make = function(obj) {
 	var stream = '';
 	var cmd;
@@ -27,14 +29,26 @@ var make = exports.make = function(obj) {
 	return stream;
 };
 
+exports.getVariables = function(obj) {
+	var rx = /\{([^}]+)\}/g;
+	var match, matches = [];
+
+	while((match = rx.exec(obj.command)) !== null) {
+		matches.push(match[1]);
+	}
+
+	return matches;
+};
+
 exports.variables = function(obj) {
 	var s = '';
 	var i;
-	var rx = /\{([^}]+)\}/gim;
-	var matches;
 
-	while((matches = rx.exec(obj.command)) !== null) {
-		s += 'var ' + matches[1] + ';\n\n';
+	var variables = this.getVariables(obj);
+	variables = _.unique(variables);
+
+	for(i = variables.length; i--; ) {
+		s += 'var ' + variables[i] + ';\n\n';
 	}
 
 	return s;
@@ -53,6 +67,23 @@ exports.it = function(obj, cb) {
 	s = 'it(\'' + obj.command + '\', function() {\n\n';
 	s += cb(obj.children);
 	s += '});\n\n';
+	return s;
+};
+
+exports.require = function(obj, cb) {
+	var s;
+	var i;
+	var variables = this.getVariables(obj);
+	if(variables.length !== 2) {
+		s = 'var ' + variables[0] + ' = require(\'' + variables[1] + '\');';
+	}
+	else if(variables.length === 1) {
+		s = 'var ' + variables[0] + ' = require(\'' + variables[0] + '\');';
+	}
+	else {
+		throw 'Require statement for ' + obj.command + ' must have one or two variables.';
+	}
+
 	return s;
 };
 
