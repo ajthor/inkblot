9d9f67f40150adeeae31f113f5062114509d223b
Adam <adam.thorpe.g@gmail.com>
2014-05-22 20:54:15 -0600

    change to async flow

--- bin/inkblot.js
+++ bin/inkblot.js
@@ -22,12 +22,6 @@ var path = require('path');
 var glob = require('glob');
 var beautify = require('js-beautify').js_beautify;
 
-var unit = require('./unit.js');
-
-// *Globals*
-var tabs = '\t';
-var spaces = '  ';
-
 // Inkblot Object
 // ==============
 var inkblot = module.exports = function(options) {
@@ -35,234 +29,188 @@ var inkblot = module.exports = function(options) {
 		// Inkblot Defaults
 		// ----------------
 		searchString: '// t:',
-		indentUsing: 'spaces',
-
 		out: './test'
 
 	}, this.options);
 
-	this.indentation = (this.options.indentUsing === 'tabs') ? tabs : spaces;
-
 };
 
 _.extend(inkblot.prototype, {
-	// Load Function
-	// -------------
-	// Loads files based on glob patterns passed as arguments. If no 
-	// file can be loaded, it throws an error. If a file IS loaded, 
-	// it runs through the file looking for inkblot comments. If it 
-	// finds any, it calls the callback function to handle them.
-	load: function(filenames, cb) {
-		if(!_.isFunction(cb)) throw 'Must supply a callback to \'search\'.';
-		if(!Array.isArray(filenames)) filenames = [filenames];
 
-		var i;
-		for(i = filenames.length; i--; ) {
-
-			glob( filenames[i], function(err, files) {
-				if(err) {
-					throw err;
-				}
-				// If any matches are found, parse them for 
-				// inkblot comments.
-				var filename;
-				var j;
-				for(j = files.length; j--; ) {
-					filename = files[j];
-					process.stdout.write('[ ' + filename + ' ]\n');
-
-					fs.readFile(filename, function(err, data) {
-						data = data.toString('utf8');
-						if(data.indexOf(this.options.searchString) === -1) {
-							console.log('No inkblot comments found in %s', filename);
-							return;
-						}
-
-						cb.call(this, filename, data);
-
-					}.bind(this));
-				}
-
-			}.bind(this));
-		}
-	},
-
-	// Compile Function 
-	// ----------------
-	compile: function(filenames) {
-		this.load(filenames, function(filename, data) {
-			var line, start, end;
-			var count = 0;
-
-			var tests = [];
-			var testObj;
-
-			var base, ext;
-			// The variable to hold the `.spec` file stream to be 
-			// written once compilation is done.
-			var stream = '';
-			
-			// Find the comments in the file.
-			for(line = ''; (start = data.indexOf(this.options.searchString)) !== -1; count++) {
-				end = data.indexOf('\n', start) + 1;
-				line = data.slice(start, end);
-				// Remove the line from the data stream so that you 
-				// don't cycle over the same comment twice.
-				data = data.replace(line, "");
-
-				// Get workable comment that we can perform 
-				// operations on and modify.
-				line = line.slice(this.options.searchString.length + 1, -1);
-
-				tests.push(line);
-
+	// Run Function
+	// ------------
+	// The entry-point into the program.
+	run: function(files) {
+		if(!Array.isArray(files)) files = [files];
+		async.each(files, this.compile.bind(this), function(err) {
+			if(err) {
+				console.log(err.message);
 			}
-
-			// Once we have the comment in a workable form, it is 
-			// time to actually generate the test spec file. We 
-			// do this by piping the commands successively into 
-			// nested functions, each writing a piece of the spec 
-			// to the stream, which will form a neat, nested 
-			// specification once written.
-			// console.log(tests);
-
-			testObj = this.generateJSON(tests);
-
-			stream = unit.make(testObj);
-
-			// Write the file to a new spec file.
-			ext = path.extname(filename);
-			base = path.basename(filename, ext);
-			
-			filename = path.join(this.options.out, base + '.spec' + ext);
-
-			stream = beautify(stream, {indent_size: 4});
-
-			fs.writeFile(filename, stream, function(err) {
-				if(err) {
-					throw err;
-				}
-				process.stdout.write(' ==> ' + filename + '\n');
-			});
-
-		}.bind(this));
+			console.log('Task completed.');
+		});
 	},
 
-	// Generate JSON Function
-	// ----------------------
-	// Interprets the comments and creates a nested JSON object with 
-	// some metadata about each test. 
-	
-	// Creates an object set up as a tree, with 'children' nodes to 
-	// facilitate nesting. After the object has been created, it will 
-	// be run through the stream writing function to actually 
-	// generate the stream to write to the spec file.
+	// Compile Function
+	// ----------------
+	// Called from `run` function, this function asynchronously 
+	// compiles the comments in a file into a test spec file. 
+	// Basically, it opens the file, finds the comments, parses them 
+	// into a spec, and writes them to disk. If there is a problem, 
+	// it will not write the file.
+	compile: function(file, done) {
+		var searchString = this.options.searchString;
+		var destination = this.options.out;
 
-	// TODO: refactor to improve DRY coding techniques. I feel I could remove about ten lines from this function.
-	generateJSON: function(tests) {
-		var id;
+		async.waterfall([
+			// Load File
+			// ---------
+			// Load the file into the stream and check to see if 
+			// there are any test comments in it. 
+			// If not, no need to continue.
+			function load(callback) {
+				process.stdout.write('[ ' + file + ' ]\n');
 
-		var i, j;
-
-		var parent = tests[0];
-		var children = [];
-
-		var result = {};
-		for(i = 0; i < tests.length; i++) {
-
-			// Test if the current line is a 'parent'. If it is, then 
-			// push all children which have accumulated to the 
-			// previous parent and set up a new parent to add 
-			// children to.
-			if(this._getLevel(tests[i]) === 0) {
-				
-				if(children.length > 0) {
-					for(j = 0; j < children.length; j++) {
-						children[j] = children[j].slice(this.indentation.length);
+				if(file.indexOf('.spec') !== -1) {
+					return callback(new Error('Cannot run inkblot on a spec file: ' + file), null);
+				}
+		
+				fs.readFile(file, {encoding: 'utf8'}, function(err, data) {
+					if(data.indexOf(searchString) === -1) {
+						callback(new Error('No inkblot comments in file: ' + file), data);
 					}
 
-					parent.children = this.generateJSON(children);
-				}
+					callback(null, data);
+				});
 
-				// Make a new parent. Default key:value pairs are 
-				// `command` and `children`. 'Command' holds the full 
-				// string in the line, minus any indentation.
-				id = _.uniqueId();
+			},
+			
+			// Parse Comments
+			// --------------
+			// Convert the comments to an array of workable commands.
+			this.findComments.bind(this),
+			
+			// Create Spec
+			// -----------
+			// Using another async function call, create a spec from 
+			// the command object.
+			this.createSpec.bind(this)
+		],
+		// Save File
+		// ---------
+		// Convert the file name into a `.spec` version and save it 
+		// to the output directory if no errors occurred 
+		// along the way.
+		function(err, result) {
+			var ext, base;
 
-				parent = result[id] = {
-					command: tests[i],
-					children: null
-				};
-
-				children = [];
-
+			if(err) {
+				console.log(err.message);
 			}
-			// This line has an indentation level higher than the 
-			// current indentation level; it is a 'child' element and 
-			// we should add it to the list of children to be added.
 			else {
-				children.push(tests[i]);
-			}
+				ext = path.extname(file);
+				base = path.basename(file, ext);
+				file = path.join(destination, base + '.spec' + ext);
 
+				result = beautify(result, {indent_size: 4});
+
+				fs.writeFile(file, result, function(err) {
+					if(err) throw err;
+					process.stdout.write('Compiled: [ ' + file + ' ]\n');
+				});
+			}
+		});
+	},
+
+	findComments: function(data, callback) {
+		var comments = [];
+		var line, start, end;
+
+		for(line = ''; (start = data.indexOf(this.options.searchString)) !== -1; ) {
+			end = data.indexOf('\n', start) + 1;
+			line = data.slice(start, end);
+			// Remove the line from the data stream so that you 
+			// don't cycle over the same comment twice.
+			data = data.replace(line, "");
+			// Get workable comment that we can perform 
+			// operations on and modify.
+			line = line.slice(this.options.searchString.length + 1, -1);
+
+			comments.push(line);
 		}
 
-		// Push any last children to the result object. This handles 
-		// any 'dangling' children that appear last in the list.
-		if(children.length > 0) {
-			for(j = 0; j < children.length; j++) {
-				children[j] = children[j].slice(this.indentation.length);
+		return callback(null, comments);
+	},
+
+	// Create Spec Function
+	// --------------------
+	// 
+	createSpec: function(comments, callback) {
+
+		var stream = '';
+
+		var obj = this.makeObject(comments);
+		// console.log(obj);
+
+		// If current level = next level : siblings
+		// If current level > next level : not a child
+		// If current level < next level : child
+
+		callback(null, stream);
+	},
+
+	// Make Object Function
+	// --------------------
+	makeObject: function(comments) {
+		if(comments.length == 0) return null;
+		var i, j;
+
+		var currentLevel = this.indentLevel(comments[0]);
+		var collected = [];
+
+		var result = {};
+
+		var id;
+
+		for(i = 0; i < comments.length; i++) {
+			console.log("i: %d - %s", i, comments[i]);
+			collected = [];
+
+			id = _.uniqueId();
+
+			result[id] = {
+				desc: comments[i],
+				children: null
 			}
 
-			parent.children = this.generateJSON(children);
+			for(j = i + 1; j < comments.length; j++) {
+				if(currentLevel >= this.indentLevel(comments[j]))
+					break;
+
+				console.log("  j: %d", j);
+				collected.push( comments.splice(j, 1) );
+
+				console.log(comments);
+			}
+
+			result[id].children = this.makeObject(collected);
+
 		}
 
 		return result;
 	},
-
-
-	generateTests: function(obj) {
-		var stream = '';
-
-		stream = unit.make(obj, stream);
-	},
-
+	
 	// Get Indentation Level
 	// ---------------------
 	// Take lines of comments and determine the indentation level of 
-	// each comment. This will determine nesting. Once this is 
-	// determined, it will be possible to interpret the sub-commands 
-	// of each comment.
-	_getLevel: function(line) {
+	// each comment. This will determine nesting.
+	indentLevel: function(comment) {
 		var level, piece;
 
-		for(level = 0; (piece = line.slice(0, 2)) == this.indentation; level++) {
-			line = line.slice(this.indentation.length);
+		for(level = 0; (piece = comment.slice(0, 2)) == '  '; level++) {
+			comment = comment.slice(2);
 		}
 
 		return level;
-	},
-
-	// Clean Comments
-	// --------------
-	// Removes test comments beginning with `searchString` found in 
-	// the file.
-	clean: function(filenames) {
-		this.load(filenames, function(filename, data) {
-			var line, index;
-			
-			for(line = ''; (index = data.indexOf(this.options.searchString)) !== -1; ) {
-				line = data.slice(index, data.indexOf('\n', index) + 1);
-
-				data = data.replace(line, "");
-			}
-
-			fs.writeFile(filename, data, function(err) {
-				if(err) {
-					throw err;
-				}
-			});
-
-		});
 	}
 
 });
