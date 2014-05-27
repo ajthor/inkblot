674e6b744b25ccd773bf22f9e751deb38e3260da
Adam <adam.thorpe.g@gmail.com>
2014-05-27 11:29:25 -0600

    add dummy file for testing purposes

---
+++ test/dummy.js
@@ -0,0 +1,14 @@
+// test: require {name:inkblot} at {path:'../bin/inkblot.js'}
+var inkblot = require('../bin/inkblot.js');
+
+// test: describe function hello
+// test:   it should be defined
+exports.hello = function() {
+
+}
+
+// test: describe function world
+// test:   it should be defined
+exports.world = function() {
+
+}
