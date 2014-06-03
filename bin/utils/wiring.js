b66bfeb2485f60d328d7945b1d795eda2f8c43ec
Adam <adam.thorpe.g@gmail.com>
2014-06-03 13:50:09 -0600

    bugfix: wiring find outer block function

--- bin/utils/wiring.js
+++ bin/utils/wiring.js
@@ -46,17 +46,23 @@ exports.getInnerBlock = function (index, block) {
 // Retreives the whole block, including the wrapper function.
 exports.getOuterBlock = function (index, block) {
 	var end, start;
-	var blockCount = 0;
+	var blockCount = 1;
 
 	var i;
 
-	for (i = index; !start || i--; ) {
+	// Find the start of the block. Basically, it works backwards 
+	// until it hits a newline character.
+	for (i = index; (i > 0) && (!start); i--) {
 		if (block[i] === '\n') {
 			start = i;
 		}
 	}
 
-	end = findMatchingBrace(block.indexOf('{', start), block);
+	end = findMatchingBrace(block.indexOf('{', start) + 1, block);
+
+	// We add (usually) three characters here to include the brace, 
+	// the parenthesis, and the semicolon.
+	end = block.indexOf(';', end) + 1;
 
 	return block.slice(start, end).trim();
 };
